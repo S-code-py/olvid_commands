bb66fd3379a8a3a96b7507a8394e8ccb84ab0930
S-code-py <sami.racaud@gmail.com>
2026-04-01 21:38:26 +0200

    t

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="olvid_command",
-    version="0.2",
+    version="0.3",
     packages=find_packages(),
     description="Integration of commands and commands group in olvid",
     author="SamBotPy",
